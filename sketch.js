04e89fb0adb15b69d08dd70084bf3532d39ff1b2
ruhi123A <asarruhi@gmail.com>
2021-04-09 14:17:44 +0800

    i wrote a comment

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    //its a bird
     bird = new Bird(100,100);
 
 }
